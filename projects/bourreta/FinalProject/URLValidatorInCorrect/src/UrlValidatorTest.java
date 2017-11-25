c958e92d726a73cebe9f53c74739b8542d4ae2e5
bourreta <bourreta@oregonstate.edu>
2017-11-24 19:01:14 -0500

    FinalProject B

--- projects/bourreta/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ projects/bourreta/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -42,39 +42,148 @@ public class UrlValidatorTest extends TestCase {
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
 	   System.out.println(urlVal.isValid("http://www.amazon.com"));
-	   
+	   System.out.println(urlVal.isValid("www.amazon.com"));
+	   System.out.println(urlVal.isValid("f3tp://www.amazon.com"));
+	   System.out.println(urlVal.isValid("3ttp://www.amazon.com"));
+	   System.out.println(urlVal.isValid("http://google.com/search?query=some+query"));
+	   System.out.println(urlVal.isValid("http://google.com/search"));
+	   System.out.println(urlVal.isValid("http://google.com/search?query=some"));
+	   System.out.println(urlVal.isValid("http://google.com/search?query"));
 	   
    }
    
    
    public void testYourFirstPartition()
    {
-	   
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       System.out.println("-------- TEST PARTITION:  SCHEME ----------");
+
+       System.out.println("Valid Schemes (all values should be true: ");
+       System.out.println("\t" + urlVal.isValid("http://www.amazon.com") + " -- http://www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("https://www.amazon.com") + " -- https://www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("ftp://www.amazon.com") + " -- ftp://www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("www.amazon.com") + " -- www.amazon.com");
+
+       System.out.println("Invalid Schemes (all values should be false: ");
+       System.out.println("\t" + urlVal.isValid("http:///www.amazon.com") + " -- http:///www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("https//:www.amazon.com") + " -- http//:www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("http:www.amazon.com") + " -- http:www.amazon.com");
+       System.out.println("\t" + urlVal.isValid("http//www.amazon.com") + " -- http//www.amazon.com");
    }
    
-   public void testYourSecondPartition(){
-	   
+   public void testYourSecondPartition()
+   {
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       System.out.println("-------- TEST PARTITION:  HOST ----------");
+
+       System.out.println("Valid Hosts (all values should be true): ");
+       System.out.println("\t" + urlVal.isValid("http://amazon.com") + " -- http://amazon.com");
+       System.out.println("\t" + urlVal.isValid("http://www.ama-zon.com") + " -- http://www.ama-zon.com");
+       System.out.println("\t" + urlVal.isValid("http://www.2amazon.com") + " -- http://www.2amazon.com");
+       System.out.println("\t" + urlVal.isValid("http://ww2.amazon.com") + " -- http://ww2.amazon.com");
+       System.out.println("\t" + urlVal.isValid("http://128.193.40.35") + " -- http://128.193.40.35");
+
+       System.out.println("Invalid Schemes (all values should be false): ");
+       System.out.println("\t" + urlVal.isValid("http://www.@mazon.com") + " -- http://www.@mazon.com");
+       System.out.println("\t" + urlVal.isValid("https://www.ama zon.com") + " -- http://www.ama zon.com");
+       System.out.println("\t" + urlVal.isValid("http://128.193.40.256") + " -- http://128.193.40.256");
    }
    
    
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
-	   {
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       String site = "http://www.amazon.com";
+       System.out.println("--testIsValid()---");
+       String testUrl = null;
+
+	   for(int i = 0; i < allPartitions.length; i++) {
 		   
+		   if(allPartitions[i] == validPartitions) {
+			   System.out.println("Testing Valid Partitions (all should be true)");
+		   }
+		   else {
+			   System.out.println("Testing Invalid Partitions (all should be false)");
+		   }
+	       for(int j = 0; j < allPartitions[i].length; j++) {
+	   			if(allPartitions[i][j] == testUrlSchemeValid || allPartitions[i][j] == testUrlSchemeInvalid) {
+       	   			System.out.println("\tTesting Scheme");
+   	   			}
+   	   			else if(allPartitions[i][j] == testHostValid || allPartitions[i][j] == testHostInvalid) {
+       	   			System.out.println("\tTesting Host");
+   	   			}
+   	   			else if(allPartitions[i][j] == testTLDValid || allPartitions[i][j] == testTLDInvalid) {
+       	   			System.out.println("\tTesting Top-Level Domain");
+   	   			}
+   	   			else if(allPartitions[i][j] == testPortValid || allPartitions[i][j] == testPortInvalid) {
+       	   			System.out.println("\tTesting Port");
+   	   			}
+   	   			else if(allPartitions[i][j] == testPathValid || allPartitions[i][j] == testPathInvalid) {
+       	   			System.out.println("\tTesting Port");
+   	   			}
+   	   			else if(allPartitions[i][j] == testQueryValid || allPartitions[i][j] == testQueryInvalid) {
+       	   			System.out.println("\tTesting Query");
+   	   			}
+   	   		for(int k = 0; k < allPartitions[i][j].length; k++){
+   			  
+   	   			if(allPartitions[i][j] == testUrlSchemeValid || allPartitions[i][j] == testUrlSchemeInvalid) {
+       	   			testUrl = site.replaceAll("(^[a-z]+://)", allPartitions[i][j][k]);
+   	   			}
+   	   			else if(allPartitions[i][j] == testHostValid || allPartitions[i][j] == testHostInvalid) {
+       	   			testUrl = site.replaceAll("www.amazon", allPartitions[i][j][k]);
+   	   			}
+   	   			else if(allPartitions[i][j] == testTLDValid || allPartitions[i][j] == testTLDInvalid) {
+       	   			testUrl = site.replaceAll(".com", allPartitions[i][j][k]);
+   	   			}
+   	   			else if(allPartitions[i][j] == testPortValid || allPartitions[i][j] == testPortInvalid) {
+       	   			testUrl = site + allPartitions[i][j][k];
+   	   			}
+   	   			else if(allPartitions[i][j] == testPathValid || allPartitions[i][j] == testPathInvalid) {
+       	   			testUrl = site + allPartitions[i][j][k];
+   	   			}
+   	   			else if(allPartitions[i][j] == testQueryValid || allPartitions[i][j] == testQueryInvalid) {
+       	   			testUrl = site + allPartitions[i][j][k];
+   	   			}
+   	   			System.out.println("\t\t"+testUrl + " - " + urlVal.isValid(testUrl));
+   	   		 }
+	      }
 	   }
    }
+   
    
    public void testAnyOtherUnitTest()
    {
 	   
    }
+   
    /**
     * Create set of tests by taking the testUrlXXX arrays and
     * running through all possible permutations of their combinations.
     *
     * @param testObjects Used to create a url.
     */
-   
-
+ 
+   	String testUrlSchemeValid[] = {"http://", "https://", "ftp://", "h3t://", ""};
+   	String testUrlSchemeInvalid[] = {"3ht://", "http:/", "http:///", ":"};
+   	
+   	String testHostValid[] = {"www.google", "3com", "google", "ama-zon", "www2.reddit", "www3.google", "en.wikipedia"};
+   	String testHostInvalid[] = {"am3_zon", "www.ama zon", "ama zon", "amazon ", " amazon", "@mazon", "www3.red;dit", "-www.oregonstate", "-oregonstate"};
+   	
+   	String testTLDValid[] = {".com", ".co.uk", ".net", ".biz", ".org", ".edu", ".gov", ".ac", ".ba", ".bb", ".cf", ".de", ".ec", ".hk", ".ma", ".pa", ".sb", ".tc", ".va", ".wf", ".ye", ".za"};
+    String testTLDInvalid[] = {".ak", ".ik", "com", "net", "biz", ".zb"};
+    
+    String testPortValid[] = {":0", ":80", ":32217", ":65535"};
+    String testPortInvalid[] = {":-1", ":65536", ":610234", ":80x", ":j80"};
+    
+    String testPathValid[] = {"/index1", "/index123", "/index/file/test", "/$78", "/index.html"};
+    String testPathInvalid[] = {"/..", "index//file", "/../file1"};
+    
+    String testQueryValid[] = {"?id=100", "?mode=test&result=unknown", "?key1=value1;key2=value2"};
+    String testQueryInvalid[] = {"?id+100", "?mode+test&result_unknown"};
+   	
+    String validPartitions[][] = {testUrlSchemeValid, testHostValid, testTLDValid, testPortValid, testPathValid, testQueryValid};
+    String invalidPartitions[][] = {testUrlSchemeInvalid, testHostInvalid, testTLDInvalid,testPortInvalid, testPathInvalid, testQueryInvalid};
+    
+    String allPartitions[][][] = {validPartitions, invalidPartitions};
 }
+
